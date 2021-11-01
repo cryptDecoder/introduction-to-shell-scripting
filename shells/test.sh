4bf85f32c25c4c827b3c48c86c6d94eefd9e8468
darkhost <cryptDecoder@gmail.com>
2021-11-01 11:54:24 +0530

    Readme updated

--- shells/test.sh
+++ shells/test.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-## declare an array variable
-echo "[+] declare array "
-declare -a List=(
-  "element 1"
-  "element 2"
-  "element 3"
-)
-for entry in "${List[@]}"; do
-  echo "$entry"
-done
 
-function welcome() {
-  echo "Welcome $1"
-}
-echo "Enter your name :"
-read name
-welcome $name
